745069c5198dc3599bc738d7133c8f23921cec58
ma-08 <77933842+ma-08@users.noreply.github.com>
2023-03-03 11:50:06 +0300

    Add files via upload

---
+++ sem12.py
@@ -0,0 +1,34 @@
+import re
+
+
+class Word:
+    allowed_languages = ['русский', 'английский']
+
+    def __init__(self, glossed: str, gloss: str, language):
+        self.glossed = glossed
+        self.gloss = gloss
+        if language not in self.allowed_languages:
+            decision = input("Языка нет в списке доступных. Желаете его добавить? [д/н]", )
+            if decision == "д":
+                self.allowed_languages.append(language)
+                self.language = language
+            else:
+                raise(ValueError("Выберите поддерживаемый язык "
+                                 f"Список поддерживаемых языков {self.allowed_languages}"))
+        else:
+            self.language = language
+
+    def __len__(self) -> int:
+        word = re.sub(r'[-.=:]', '', self.glossed)
+        return len(word)
+
+    def __str__(self) -> str:
+        return f'{self.glossed}\n{self.gloss}'
+
+    def count_morphemes(self, blocks):
+        return len(self.glossed.split(blocks))
+
+    __repr__ = __str__
+
+
+print(Word('мам-а=aaa.a', 'mother-NOM.SG', 'русский').count_morphemes('.-='))
